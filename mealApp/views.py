fcefc5df5c6c382f99b76674da108256f77af066
tx-smitht <tx.smiththomas@gmail.com>
2022-10-29 18:53:28 -0600

    learning and implementing templates

--- mealApp/views.py
+++ mealApp/views.py
@@ -1,9 +1,9 @@
-from http.client import HTTPResponse
-from urllib import response
+
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def indexPageView(request):
     sOutput = 'Hello World!'
-    return HttpResponse(sOutput)
+    return render(request, 'mealApp/index.html')
